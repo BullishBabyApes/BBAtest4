a0d3a6aeb5b71049025a3388653cfb222ce7c9c9
BullishBabyApes <99092765+BullishBabyApes@users.noreply.github.com>
2022-02-15 13:02:30 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 3) {
-      newMintAmount = 3;
+    if (newMintAmount > 9) {
+      newMintAmount = 9;
     }
     setMintAmount(newMintAmount);
   };
